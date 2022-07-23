3d340994a0655416e35e9df2dd5350cc6ae55f08
pratik7588 <pratik@unacores.com>
2022-07-23 22:10:05 +0530

    added statment programs

---
+++ testNGPrograms/statementSwitch/Switch2.java
@@ -0,0 +1,22 @@
+package statementSwitch;
+
+public class Switch2 {
+
+	public static void main(String[] args) 
+	{
+	 String mobile="iPhone";
+	 switch (mobile)
+	 {
+		 case"samsung":System.out.println("Buy a samsung phone");
+		 break;
+		 case"iPhone":System.out.println("Buy a iPhone");
+		 break;
+		 case"Redmi":System.out.println("Buy a Redmi phone");
+		 break;
+		 default:System.out.println("In default");
+		 
+	 }
+
+	}
+
+}
